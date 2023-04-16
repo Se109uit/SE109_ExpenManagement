c31725e8f7d5c013bb70c12edcc74d3d67fd1264
Sang <75106540+sang1833@users.noreply.github.com>
2023-04-16 16:16:47 +0700

    i18n

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import { Container, Button } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 
-function App() {
-  const [count, setCount] = useState(0)
+const LanguageSwitcher = () => {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lang) => {
+    i18n.changeLanguage(lang);
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <Button variant="primary" onClick={() => changeLanguage('en')}>{t('english')}</Button>{' '}
+      <Button variant="secondary" onClick={() => changeLanguage('vi')}>{t('vietnamese')}</Button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+const App = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Container>
+      <LanguageSwitcher />
+      <h1>{t('greeting')}</h1>
+      <p>{t('description')}</p>
+    </Container>
+  );
+};
+
+export default App;
---
+++ src/i18n.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        resources: {
+        en: {
+            translation: {
+            greeting: 'Hello',
+            description: 'Welcome to my website!',
+            },
+        },
+        vi: {
+            translation: {
+            greeting: 'Xin chào',
+            description: 'Chào mừng đến với trang web của tôi!',
+            },
+        },
+        },
+        fallbackLng: 'en',
+        interpolation: {
+        escapeValue: false,
+        },
+    });
+
+
+
+
