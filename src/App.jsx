3a496736734d0febe6206b385bd97e2ac60f4efd
Sang <75106540+sang1833@users.noreply.github.com>
2023-04-21 17:55:45 +0700

    just login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,15 @@ import React, { useState } from 'react';
 import { Container, Button } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import { Counter } from './features/counter/counter';
-
-const LanguageSwitcher = () => {
-  const { t, i18n } = useTranslation();
-
-  const changeLanguage = (lang) => {
-    i18n.changeLanguage(lang);
-  };
-
-  return (
-    <div>
-      <Button variant="primary" onClick={() => changeLanguage('en')}>{t('english')}</Button>{' '}
-      <Button variant="secondary" onClick={() => changeLanguage('vi')}>{t('vietnamese')}</Button>
-    </div>
-  );
-};
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Login from './components/Login/Login';
+import './App.css'
 
 const App = () => {
-  const { t } = useTranslation();
-
   return (
-    <Container>
-      <Counter className = "pb-4"></Counter>
-      <LanguageSwitcher />
-      <h1>{t('greeting')}</h1>
-      <p>{t('description')}</p>
-    </Container>
+    <div className='App'>
+      <Login></Login>
+    </div>
   );
 };
 
